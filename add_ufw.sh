e1d14002be0f7739af707497b17d9af4440a6e03
2Kappa-Mikey <2firewalker@gmail.com>
2024-10-19 09:28:47 +0300

    Update add_ufw.sh

--- add_ufw.sh
+++ add_ufw.sh
@@ -1,23 +1,7 @@
+# Отключение двухстороннего пинга в ufw
 echo "Отключение двухстороннего пинга в ufw..."
-
-# Проверяем, есть ли уже правила для блокировки ICMP
-if ! grep -q "icmp --icmp-type echo-request" /etc/ufw/before.rules; then
-    echo "Добавляем правила блокировки ICMP..."
-
-    # Добавляем правила в before.rules
-    cat <<EOT >> /etc/ufw/before.rules
-# Отключение ICMP echo request и reply
--A ufw-before-input -p icmp --icmp-type echo-request -j DROP
-EOT
-
-    # Перезагружаем UFW для применения изменений
-    ufw reload
-
-    if [ $? -eq 0 ]; then
-        echo "ICMP блокировка добавлена и UFW перезагружен."
-    else
-        echo "Ошибка при перезагрузке UFW."
-    fi
-else
-    echo "ICMP уже заблокирован."
+if grep -q "^-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT" /etc/ufw/before.rules; then
+    sed -i 's/^-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/-A ufw-before-input -p icmp --icmp-type echo-request -j DROP/' /etc/ufw/before.rules
+	ufw reload
+    echo "ICMP отключён."
 fi
